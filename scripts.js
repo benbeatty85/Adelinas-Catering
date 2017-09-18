e33543f95d1ced4d8a2fc81c1f40100cd022e540
benbeatty85 <benbeatty85@gmail.com>
2017-09-18 15:34:18 -0400

    updated pictures and put in page loader

--- scripts.js
+++ scripts.js
@@ -1,6 +1,16 @@
+//toggle menu bar
+
 $(document).ready(function(){
         $('.handle').on('click', function(e){
             e.preventDefault();
             $('nav ul').slideToggle();
         });
     });
+
+//page loader
+
+var overlay = document.getElementById("overlay");
+
+window.addEventListener('load', function () {
+  overlay.style.display = 'none';
+});
