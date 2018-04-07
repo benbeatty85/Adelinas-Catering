2406617fedaaa89d3a129fdeb08c5668bc2a0b2d
benbeatty85 <benbeatty85@gmail.com>
2018-04-07 09:53:08 -0400

    Added favicon and smooth scrolling

--- scripts.js
+++ scripts.js
@@ -14,3 +14,20 @@ var overlay = document.getElementById("overlay");
 window.addEventListener('load', function () {
   overlay.style.display = 'none';
 });
+
+
+ //Smooth Scrolling for navigation
+
+
+    $(document).ready(function(){
+        $('a[href*="#"]').on('click',function (e) {
+            e.preventDefault();
+            var target = this.hash;
+            $target = $(target);
+            $('html, body').stop().animate({
+                'scrollTop':  $target.offset().top 
+            }, 800, 'swing', function () {
+                window.location.hash = target;
+            });
+        });
+    });
