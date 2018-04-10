a659775a868e9367ddb5c623dabc241bdc7d4ad0
benbeatty85 <benbeatty85@gmail.com>
2018-04-09 21:27:11 -0400

    New Website

--- scripts.js
+++ scripts.js
@@ -3,9 +3,16 @@
 $(document).ready(function(){
         $('.handle').on('click', function(e){
             e.preventDefault();
-            $('nav ul').slideToggle();
+            $('nav ul').slideToggle(-1000);
         });
     });
+
+$(document).ready(function() {
+    $('.menu-link').on('click', function (e) {
+        e.preventDefault();
+        $('nav ul').hide();
+    })
+})
 
 //page loader
 
@@ -31,3 +38,48 @@ window.addEventListener('load', function () {
             });
         });
     });
+
+
+      // Scrolling effect on desktop navigation
+
+      $(window).on("scroll", function() {
+            if($(window).scrollTop()) {
+                  $('nav').addClass('white');
+            }
+
+            else {
+                  $('nav').removeClass('white');
+            }
+      })
+
+    //slideshow
+
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1} 
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none"; 
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block"; 
+  dots[slideIndex-1].className += " active";
+}
